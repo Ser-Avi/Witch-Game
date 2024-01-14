0bcd1e7b679326d8afe4fcdd6c18f7d803733153
Ser-Avi <avi_serebrenik@yahoo.com>
2024-01-14 00:29:28 -0600

    Added dialogue up to scene 5A

--- Assets/Scripts/LevelChanger.cs
+++ Assets/Scripts/LevelChanger.cs
@@ -10,6 +10,11 @@ public class LevelChanger : MonoBehaviour
         fadeAnimator.SetTrigger("Fade Out");
     }
 
+    public void FadeOutBool()
+    {
+        fadeAnimator.SetBool("Fade Out", true);
+    }
+
     public void OnFadeComplete()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -18,6 +23,12 @@ public class LevelChanger : MonoBehaviour
     public void FadeIn()
     {
         fadeAnimator.SetTrigger("Fade In");
+
+    }
+
+    public void FadeInBool()
+    {
+        fadeAnimator.SetBool("Fade Out", false);
     }
 
     public void LoadNext()
